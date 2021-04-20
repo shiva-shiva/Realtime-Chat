f6525dc05953a47b6d13c97429b2eda14ade176a
shiva-shiva <shivasabokdast@gmail.com>
2021-04-20 16:41:10 -0400

    join.js

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React , {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Join = ()=>{
+ const [name, setName] = useState('');
+ const [room, setRoom] = useState('');
     return(
-        <h1>Join</h1>
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 class="heading">Join</h1>
+                <div><input placeholder="" className="joinInput" type="text" onChange={}/></div>
+                <div><input placeholder="" className="joinInput" type="text" onChange={}/></div>
+                <link>
+                  <button className="button" type="submit">Sign In</button>
+                </link>
+
+            </div>
+        </div>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -8,8 +8,13 @@ const io = socketio(server);
 const router = require('./router')
 
 io.on('connection', (socket)=>{
-   console.log('we have a new connection')
+ console.log('we have a new connection!!!')
+ socket.on('disconnect',()=>{
+     console.log('User had left!!!');
+ })
 });
+
+
 app.use(router);
 server.listen(PORT, ()=>{
     console.log(`Server has started on port ${PORT}`);
