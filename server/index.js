c60d3289eb2e9f17ced7008c9c9c23159bd8b80e
shiva-shiva <shivasabokdast@gmail.com>
2021-04-20 16:11:01 -0400

    import socket.io

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const socketio = require('socket.io');
+const http = require('http');
+const PORT = process.env.PORT || 5000
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const router = require('./router')
+
+app.use(router);
+server.listen(PORT, ()=>{
+    console.log(`Server has started on port ${PORT}`);
+})
