85b228a2519ef4d4b79701e5232e626f65ca5c10
shiva-shiva <shivasabokdast@gmail.com>
2021-04-21 10:39:04 -0400

    add chat page

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client'
 
-const Chat = ()=>{
+
+let socket;
+
+const Chat = ({location})=>{
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    useEffect(()=>{
+        const {name, room} = queryString.parse(location.search);
+        socket = io('localhost:5000');
+        setName(name);
+        setRoom(room);
+    });
     return(
         <h1>Chat</h1>
     )
